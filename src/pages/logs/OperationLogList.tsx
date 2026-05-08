809266b556fb4ec718bcf43548ddce9106ae4126
414871700-web <414871700@qq.com>
2026-05-09 01:01:45 +0800

    add OperationLogList component (force)

---
+++ src/pages/logs/OperationLogList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OperationLogList: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">操作日志列表</h1>
+      <p>功能开发中...</p>
+    </div>
+  );
+};
+
+export default OperationLogList;
